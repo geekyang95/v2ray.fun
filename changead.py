bd9e88307e66d1ab0b92df146e044fc500b4cab5
Molly Lau <windowsbeta@outlook.com>
2019-02-17 22:58:40 +0800

    添加更新广告过滤插件功能的交互

--- changead.py
+++ changead.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import readjson
-import writejson
+import readjson, writejson, os, utils
 
 rules = readjson.ConfRouting[u"settings"][u"rules"]
+ad_rules_file = "/usr/bin/v2ray/h2y.dat"
 
 if rules[1][u"outboundTag"] == "direct":
     if_open_ad_function = "广告拦截功能： 未开启"
@@ -17,6 +17,7 @@ print(if_open_ad_function)
 print("")
 print("1. 开启")
 print("2. 关闭")
+print("3. 更新广告过滤策略")
 
 choice = raw_input("请选择： ")
 
@@ -24,3 +25,10 @@ if choice == "1":
     writejson.WriteAD("on")
 elif choice == "2":
     writejson.WriteAD("off")
+elif choice == "3":
+    if os.path.exists(ad_rules_file):
+        os.remove(ad_rules_file)
+        download_files(f_url = "https://raw.githubusercontent.com/ToutyRater/V2Ray-SiteDAT/master/geofiles/h2y.dat", f_name = "h2y.dat")
+        move_files("h2y.dat", "/usr/bin/v2ray/")
+    else:
+        print ("未找到广告过滤文件！")
